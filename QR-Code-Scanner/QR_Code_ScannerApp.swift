475c602fa0b9b41d5aed862bf0d7aae89fa1949a
dekapd99 <dekaprimatio@gmail.com>
2023-09-25 10:58:50 +0700

    Initial Commit

---
+++ QR-Code-Scanner/QR_Code_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_Code_ScannerApp.swift
+//  QR-Code-Scanner
+//
+//  Created by Deka Primatio on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct QR_Code_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
