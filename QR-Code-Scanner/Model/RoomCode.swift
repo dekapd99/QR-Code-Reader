5869b7cebeea20fca005ab0bfa1eab32563a72e0
dekapd99 <dekaprimatio@gmail.com>
2023-09-29 08:47:08 +0700

    Home Page UI

---
+++ QR-Code-Scanner/Model/RoomCode.swift
@@ -0,0 +1,20 @@
+//
+//  RoomCode.swift
+//  QR-Code-Scanner
+//
+//  Created by Deka Primatio on 27/09/23.
+//
+
+import SwiftUI
+
+struct RoomCode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoomCode_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomCode()
+    }
+}
